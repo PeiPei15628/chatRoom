60f84a46490d278c106b044a70f777a5c6b6dc9d
os <os@osdeMac-mini.lan>
2019-10-24 09:22:58 +0800

    chat

---
+++ server.js
@@ -0,0 +1,94 @@
+const express=require('express');
+const app=express();
+const server=require('http').createServer(app);
+const io=require('socket.io').listen(server);
+const _=require('underscore');
+
+app.use(express.static('./src'));
+
+server.listen(8088,(err)=>{
+	if(err)
+		throw err;
+	else{
+		console.log('成功监听8088端口');
+	}
+});
+
+
+// 检查昵称是否重复
+// io.sockets.sockets 返回所有在线的socket对象集合
+var checkNickname = function(name){
+    for(var k in io.sockets.sockets){
+        if(io.sockets.sockets.hasOwnProperty(k)){
+            if(io.sockets.sockets[k] && io.sockets.sockets[k].nickname == name){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+//在线用户数量
+//console.log(io.eio.clientsCount);
+/*****
+io.eio.clientsCount  // 链接数量
+socket.adapter.rooms // 所有房间
+socket.nsp.connected // 所有链接
+***/
+
+io.on('connection',(socket)=>{
+	//用户登录
+	socket.on('login',(username)=>{
+		console.log(socket)
+		if(checkNickname(username)){  //用户名已存在
+			socket.emit('nickname');
+		}else{
+			//socket.id;   唯一
+			socket.nickname=username;
+			socket.emit('loginSuccess');   //登录成功
+			var nums=io.eio.clientsCount;  //在线人数
+			io.sockets.emit('system',username,nums,'login');
+		}
+	});
+
+	//用户断开连接
+	socket.on('disconnect',()=>{
+		if(socket.nickname!= null){
+			var nums=io.eio.clientsCount;  //在线人数
+			socket.broadcast.emit('system',socket.nickname,nums,'logout')
+		}
+	});
+
+	//用户发广播消息
+	socket.on('msg',(msg)=>{
+		//socket.broadcast.emit('newMsg',socket.nickname,msg);
+		//私发消息 _.findWhere(io.sockets.sockets,{nickname:'burt'}).emit();
+		io.sockets.emit('newMsg',socket.nickname,msg);
+	});
+	//用户发广播图片消息
+	socket.on('imgMsg',(addr)=>{
+		io.sockets.emit('newImg',socket.nickname,addr);
+	});
+	//用户发广播的本地图片
+	socket.on('mySelfImg',(addr)=>{
+		io.sockets.emit('mySelfImg',socket.nickname,addr);
+	});
+	
+});
+
+
+//后台接受的图片是base64编码字符串，要将其转换为图片
+/****
+var imgData = req.body.imgData; //接收前台POST过来的base64
+//过滤data:URL
+var base64Data = imgData.replace(/^data:image\/\w+;base64,/, "");
+var dataBuffer = new Buffer(base64Data, 'base64');
+fs.writeFile("out.png", dataBuffer, function(err) {
+	if(err){
+	  res.send(err);
+	}else{
+	  res.send("保存成功！");
+	}
+});
+
+****/
---
+++ src/js/index.js
@@ -0,0 +1,179 @@
+function getdate() {
+	const dt = new Date();
+	const y = dt.getFullYear();
+	const m = dt.getMonth() + 1;
+	const d = dt.getDate();
+	const hh = dt.getHours().toString().padStart(2, 0);
+	const mm = dt.getMinutes().toString().padStart(2, 0);
+	const ss = dt.getSeconds().toString().padStart(2, 0);
+	const time = y + '-' + m + '-' + d + ' ' + hh + ':' + mm + ':' + ss;
+	return time;
+}
+let addr;
+for (let i = 1; i <= 69; i++) {
+	addr = `images/emoji/${i}.gif`;
+	let li = `<li class="emojiImgM"><img src="${addr} "alt="" class="liImg"></li>`
+	$('.emoji').append($(li))
+}
+function toBottom(){
+	let middleHeight=$('.middle').height()
+	let ul=document.getElementsByClassName('ul')[0]
+	console.log(middleHeight,ul.scrollHeight)
+	if(ul.scrollHeight>middleHeight){
+		$('.middle').scrollTop(ul.scrollHeight-middleHeight)
+	}
+}
+$('#login').click(function(e) {
+	e.stopPropagation();
+	let username = $('.userName').val().trim();
+	let sendCont;
+	if (!username) {
+		return
+	}
+	// 建立socket连接
+	let socket = io.connect('localhost:8088');
+	socket.on('connect', () => {
+		socket.emit('login', username);
+	})
+	
+	$('.xinxin').click(function(e) {
+		e.stopPropagation()
+		$(".emoji").slideToggle();
+	})
+	$('body').click(function(e) {
+		e.stopPropagation()
+		$(".emoji").hide();
+	})
+
+	$('ul.emoji').on('click', 'img', function(e) {
+		e.stopPropagation()
+		socket.emit('imgMsg', e.target.currentSrc)
+		console.log(e, 2222)
+		$(".emoji").hide();
+	})
+
+	// 用户名存在
+	socket.on('nickname', () => {
+		$('.tips').fadeIn().delay(2000).fadeOut()
+	})
+	// 登陆成功
+	socket.on('loginSuccess', () => {
+		$('.mask').hide();
+		$('.chatwindow').show();
+		let middleHeight = $('.chatwindow').height() - $('.bottom').height() - $('.top').height()
+		$('.middle').css({
+			"height": middleHeight + 'px',
+			"overflow": "hidden",
+			"overflow-y": "auto"
+		})
+		// 发送文字消息
+		$('.send').click(function() {
+			sendCont = $('.sendContent').val();
+			if (!sendCont) {
+				$('.alertBox').fadeIn().delay(1000).fadeOut();
+				return;
+			}
+			socket.emit('msg', sendCont)
+			$('.sendContent').val('')
+		})
+		// 发送本地图片
+		$('.uploadPic').click(function() {
+			$('#upload').click();
+			$('#upload').change(function(e) {
+				var file = this.files[0];
+				// 确认选择的文件是图片                
+				if (file.type.indexOf("image") == 0) {
+					var reader = new FileReader();
+					reader.readAsDataURL(file);
+					reader.onload = function(e) {
+						// 图片base64化
+						var newUrl = this.result;
+						// let li = `<li>
+						// 			<img src="${newUrl}" alt="" class="liImg">
+						// 		</li>`
+						// $('.ul').append($(li))
+
+						socket.emit('mySelfImg', newUrl);
+					};
+				}
+			});
+
+		})
+	})
+	// 系统广播 用户、人数、上/下线
+	socket.on('system', (username, num, operation) => {
+		console.log(username, num, operation)
+		$('.onLineNum').text(num);
+		let time = getdate();
+		let opt;
+		if (operation == "login") {
+			opt = "上线"
+		} else if (operation == "logout") {
+			opt = "下线"
+		}
+		let li = `<li class="announce">
+					${username} 在${time} ${opt}
+				</li>`
+		$('.ul').append($(li))
+	})
+	// 系统广播发送消息
+	socket.on('newMsg', (Tusername, sendCont) => {
+		console.log(Tusername, sendCont);
+		if (username == Tusername) {
+			let li =
+				`<li>
+						<div class="liNameL">${Tusername}:</div>
+						<div class="liCont">${sendCont}</div>
+					</li>`
+			$('.ul').append($(li))
+		} else {
+			let li =
+				`<li class="liR">
+						<div>:${Tusername}</div>
+						<div class="liContR">${sendCont}</div>
+					</li>`
+			$('.ul').append($(li))
+		}
+	})
+	// 本地图片
+	socket.on('mySelfImg', (name, addr) => {
+		if (username == name) {
+			let li =
+				`<li>
+						<div class="liNameL">${name}:</div>
+						<div class="liCont"><img src="${addr}" alt="" class="liImg"></div>
+					</li>`
+			$('.ul').append($(li))
+		} else {
+			let li =
+				`<li class="liR">
+						<div>:${name}</div>
+						<div class="liContR"><img src="${addr}" alt="" class="liImg"></div>
+					</li>`
+			$('.ul').append($(li))
+		}
+
+	})
+	// 表情
+	socket.on('newImg', (name, addr) => {
+		console.log(name, addr)
+		if (username == name) {
+			let li =
+				`<li>
+						<div class="liNameL">${name}:</div>
+						<div class="liCont"><img src="${addr}" alt="" class="liImg"></div>
+					</li>`
+			$('.ul').append($(li))
+		} else {
+			let li =
+				`<li class="liR">
+						<div>:${name}</div>
+						<div class="liContR"><img src="${addr}" alt="" class="liImg"></div>
+					</li>`
+			$('.ul').append($(li))
+		}
+		toBottom()
+
+	})
+
+})
